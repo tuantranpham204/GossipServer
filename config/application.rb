9d3292aa2245db094e12d99dc26aeaf8ff1d9cc8
tuantranpham204 <12a1tranphamtuan@gmail.com>
2026-02-04 15:00:59 +0700

    feat:internationalization

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,9 @@ module GossipServer
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    # Allow these locales (e.g., English and Vietnamese)
+    config.i18n.available_locales = [ :en, :vi ]
+    # Set default locale
+    config.i18n.default_locale = :en
   end
 end
