b39186ee154d4d487281086514aead5f08efa783
tuantranpham204 <12a1tranphamtuan@gmail.com>
2026-02-05 16:11:51 +0700

    feat: apply denylisting strategy for jwt revocation:

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -316,4 +316,22 @@ Devise.setup do |config|
   # When set to false, does not sign a user in automatically after their password is
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
+  #
+  #
+  # ==> Configuration for JWT
+  config.jwt do |jwt|
+    jwt.secret = ENV.fetch("SYSTEM_JWT_SECRET") || Rails.application.credentials.secret_key_base
+
+    # Create a token for these requests
+    jwt.dispatch_requests = [
+      [ "POST", %r{^/users/sign_in$} ],
+      [ "POST", %r{^/users/sign_up$} ]
+    ]
+    # "Invalidate the token for these requests"
+    jwt.revocation_requests = [
+      [ "DELETE", %r{^/users/sign_out$} ]
+    ]
+
+    jwt.expiration_time = ENV.fetch("SYSTEM_JWT_EXPIRATION_HOURS", 24).to_i.hours
+  end
 end
