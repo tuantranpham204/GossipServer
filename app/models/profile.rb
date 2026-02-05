e55721e854dfe9ffa630db2f164dfae80dfae59b
tuantranpham204 <12a1tranphamtuan@gmail.com>
2026-02-05 18:03:58 +0700

    feat:validations for users and profiles

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,7 @@
 class Profile < ApplicationRecord
+  VALID_NAME_REGEX = /\A[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžæœÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ ,.'-]+\z/
   belongs_to :user
+
+  validates :name, presence: true, length: { minimum: 1, maximum: 50 }, format: { with: VALID_NAME_REGEX }
+  validates :surname, presence: true, length: { minimum: 1, maximum: 50 }, format: { with: VALID_NAME_REGEX }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
+  # Validation RegEx
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_USERNAME_REGEX = /\A(?![._-])(?!.*[._-]{2})[a-zA-Z0-9._-]+(?<![._-])\z/
+  VALID_PASSWORD_REGEX = /\A(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\S+$).{8,20}\z/
+
+  # Roles
   ROLES = {
     USER: 1,
     ADMIN: 2
@@ -11,4 +17,8 @@ class User < ApplicationRecord
 
   has_one :profile, dependent: :destroy, autosave: true
   accepts_nested_attributes_for :profile
+
+  # Validations
+  validates :username, presence: true, length: { minimum: 5, maximum: 30 }, uniqueness: { case_sensitive: false }, format: { with: VALID_USERNAME_REGEX }
+  validates :password, presence: true, length: { minimum: 6, maximum: 20 }, format: { with: VALID_PASSWORD_REGEX }
 end
