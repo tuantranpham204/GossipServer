1fb45f5ff4f0b7dcdc8a8be6109c7e52e29fad3e
tuantranpham204 <12a1tranphamtuan@gmail.com>
2026-02-26 16:42:13 +0700

    fix: remove redundant spaces

--- app/controllers/concerns/api/v1/notification_concern.rb
+++ app/controllers/concerns/api/v1/notification_concern.rb
@@ -4,10 +4,6 @@ module Api::V1::NotificationConcern
       def create_notification(user_id: nil, actor_id: nil, notification_type: nil, content: nil)
         @notification = Notification.new(user_id: user_id, actor_id: actor_id, notification_type: notification_type)
         authorize @notification, :create?, policy_class: Api::V1::NotificationPolicy
-        if @notification.save!
-          @notification
-        else
-          nil
-        end
+        @notification.save! ? @notification : nil
       end
 end
